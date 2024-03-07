57e69d92d5c481f3a4c6bc38636427d577f9f55a
Jordan <jordan@hrvy.uk>
2024-03-07 21:08:02 +0000

    initial version

---
+++ README.md
@@ -0,0 +1,39 @@
+<!-- markdownlint-disable MD033 MD041 -->
+
+Python library for controlling Arcam Solo devices via a serial network bridge (ser2net)
+
+Based on the implementation of [aiopioneer](https://github.com/crowbarz/aiopioneer)
+
+## Features
+
+- Implemented in asyncio.
+- Maintains single continuous telnet session to ser2net server, reconnecting where required
+- Eliminates need to poll as library will decode status messages from the Hi-Fi unit
+- Supports power on/off
+- Supports changing source
+- Supports sending custom IR commands
+- Automatically sets the time on startup (if still supported by your software version)
+
+## Source list
+
+| ID | Name
+| -- | ---
+| 00 | N/A
+| 01 | FM
+| 02 | DAB
+| 03 | TAPE
+| 04 | AV
+| 05 | N/A
+| 06 | N/A
+| 07 | AM
+| 08 | GAME
+| 09 | USB
+| 10 | CD
+| 11 | TV
+| 12 | AUX
+
+## References
+
+- aiopioneer: [https://github.com/crowbarz/aiopioneer](https://github.com/crowbarz/aiopioneer)
+- rs232server: [https://github.com/arfoll/rs232server](https://github.com/arfoll/rs232server)
+- Arcam Solo Mini rs232 protocol: [https://www.arcam.co.uk/ugc/tor/solomini/RS232/solomini_rs232.pdf](https://www.arcam.co.uk/ugc/tor/solomini/RS232/solomini_rs232.pdf)
---
+++ test.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+""" Pioneer AVR testing script. """
+# pylint: disable=import-error,broad-except
+
+import asyncio
+import logging
+import sys
+import getopt
+
+from pyarcamsolo import ArcamSolo
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def main(argv):
+    """ Main loop. """
+    _LOGGER.debug(">> main()")
+
+    host = ""
+    try:
+        opts, _ = getopt.getopt(argv, "hp:v", ["host=", "port="])
+    except getopt.GetoptError:
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--host"):
+            host = arg
+        if opt in ("-p", "--port"):
+            port = arg
+
+    if host == "" or port == "":
+        _LOGGER.fatal("Host or port not specified.")
+        sys.exit(2)
+
+    arcam = ArcamSolo(
+        host=host,
+        port=int(port)
+    )
+
+    try:
+        await arcam.connect()
+    except Exception as e:  # pylint: disable=invalid-name
+        print(f"Could not connect to arcam: {type(e).__name__}: {e.args}")
+        return False
+
+
+    #await arcam.turn_on()
+    while True:
+        try:
+            # await arcam.send_ir_command("volume_plus")
+            await asyncio.sleep(10)
+            for z in arcam.zones.items():
+                for a in z[1].items():
+                    print(f"{z[0]}: {a[0]} - {a[1]}")
+        except KeyboardInterrupt:
+            #await arcam.turn_off()
+            await arcam.shutdown()
+            break
+
+    return True
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s %(levelname)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    rc = asyncio.run(main(sys.argv[1:]))  ## pylint: disable=invalid-name
+    exit(0 if rc else 1)
